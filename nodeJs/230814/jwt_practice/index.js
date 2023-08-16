402d7906e9f59c7e9d26871f45d796c144fb6071
SangNyungLee <lsy6747@gmail.com>
2023-08-16 10:57:55 +0900

    수정

--- nodeJs/230814/jwt_practice/index.js
+++ nodeJs/230814/jwt_practice/index.js
@@ -20,30 +20,36 @@ app.get('/token', (req,res)=>{
         const token = req.headers.authorization.split(" ")
         try {
             const result = jwt.verify(token[1], SECRET)
-            if(result.id == userInfo.id){
+            if(result.id === userInfo.id){
                 res.json({result : true, name : userInfo.name})
             }
         } catch (error) {
             console.log(error)
-            res.json({result, msg:'인증된 회원이 아닙니다.'});
+            res.json({result:false, msg:'인증된 회원이 아닙니다.'});
         }
     }else{
         res.redirect('/login');
     }
 })
 app.post('/login', (req,res)=>{
+    console.log("왓나")
     try {
         //객체 구조분해할당
-        const {id,pw} = req.body
-        const {id : uId,pw : uPw} = userInfo
+        console.log("맞나", req.body)
+        const {id,pw} = req.body;
+        const {id : uId,pw : uPw} = userInfo;
         if(id === uId && pw === uPw){
             //로그인 할 때 토큰 만들어줌
-            const token = jwt.sign({id}, SECRET)
-            res.json({result:true, token}) 
+            const token = jwt.sign({id}, SECRET);
+            res.json({result:true, token}) ;
         }else{
+            console.log(req.body)
+            console.log("스탯에러")
             res.status(404).json({ result: false, message: '로그인 정보가 올바르지 않습니다' });
-        }
+            res.send({result : true, message : "틀렷음"})
+        }  
     } catch (error) {
+        console.log("에러임")
         console.log(error)
     }
 })
