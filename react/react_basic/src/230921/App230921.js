5c1bca835b4303a802b64c13d2e945f06ac1394e
SangNyungLee <lsy6747@gmail.com>
2023-09-21 20:35:08 +0900

    LifeCycle

--- react/react_basic/src/230921/App230921.js
+++ react/react_basic/src/230921/App230921.js
@@ -1,9 +1,10 @@
-import ToggleFunction from "./ToggleFunction";
+// import ToggleFunction from "./ToggleFunction";
 // import Prac1 from "./Prac1";
 // import Prac2 from "./Prac2";
 // import Prac3 from "./Prac3";
 // import Prac4 from "./Prac4";
-import Prac5 from "./Prac5";
+// import Prac5 from "./Prac5";
+import LifeCylceClass from "./LifeCycleClass";
 function App230921() {
   return (
     <>
@@ -12,7 +13,8 @@ function App230921() {
       {/* <Prac2 /> */}
       {/* <Prac3 /> */}
       {/* <Prac4 /> */}
-      <Prac5 />
+      {/* <Prac5 /> */}
+      <LifeCylceClass />
     </>
   );
 }
