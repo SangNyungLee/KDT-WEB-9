58936a2f8391d58fbe57f81263bad6b2edfa7a7e
SangNyungLee <lsy6747@gmail.com>
2023-09-28 17:08:52 +0900

    miniproject

---
+++ miniproject/client/src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import "./App.css";
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+
+function App() {
+  return (
+    <>
+      <Header />
+    </>
+  );
+}
+
+export default App;
---
+++ miniproject/server/index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const PORT = 8000;
+const controller = require("./controller/index");
+const db = require("./models");
+
+app.set("view engine", "ejs");
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/todos", controller.get_todo);
+app.post("/todo", controller.post_todo);
+app.patch("/todo/:todoId", controller.patch_todo);
+app.delete("/todo/:todoId", controller.delete_todo);
+
+db.sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => {
+    console.log(`http://localhost:${PORT}`);
+  });
+});
