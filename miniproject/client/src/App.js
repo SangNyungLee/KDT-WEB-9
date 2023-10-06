77b35dd0a090a8a9051b7c8f9fe7e52a38d1060d
SangNyungLee <lsy6747@gmail.com>
2023-10-07 01:33:28 +0900

    todo

--- miniproject/client/src/App.js
+++ miniproject/client/src/App.js
@@ -1,5 +1,5 @@
 import logo from "./logo.svg";
-import "./App.css";
+// import "./App.css";
 import axios from "axios";
 import React, { useState, useEffect } from "react";
 import Header from "./Header";
